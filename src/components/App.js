14f4f2fb000e2174e06b5bef52924a7b512a8649
John Suico <johnsuico@johns-mbp.lan>
2020-07-04 08:05:42 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DateTime from './DateTime';
+
+function App() {
+  return (
+    <div>
+      <DateTime />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DateTime.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './DateTime.css';
+
+function DateTime() {
+  return (
+    <div>
+      <div className="container">
+        <div className="app-container">
+          <h1 className="title">Date Time App</h1>  
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DateTime;
